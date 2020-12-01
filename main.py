b4fe2e718c219cd9b49af6535c9bb38b0db8ad0c
danielcieslinski <daniel98c@gmail.com>
2020-12-01 16:35:27 +0100

    TODO: finish game mode

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 DEBUG = True
+from itertools import groupby, repeat
+
+
+
+def all_element_eq(l):
+    return all(x == l[0] for x in l)
 
 class Watersort:
-    def __init__(self,beaker_size, beakers):
+    def __init__(self,beaker_size, beakers, game_mode=False):
         """
         :param size: max size of the beaker
         :param beakers:
@@ -9,10 +15,21 @@ class Watersort:
         """
         self.beaker_size = beaker_size
         self.beakers = beakers
+        self.moved = False
+
+        if game_mode: self.game()
+
+    def game(self):
+        while True:
+            print('BOARD:')
+            print(self)
+            print('-------')
+            a,b = input('Enter valid move: <beaker_0> <beaker_1>')
+            self.move(input())
 
     def __move(self,an, bn):
         self.beakers[bn].append(self.beakers[an].pop(-1))
-        return True
+        self.moved = True
 
     def move(self,an, bn):
         """
@@ -20,34 +37,57 @@ class Watersort:
         :param bn: beaker to move to
         :return: True if success, false otherwise == move disallowed
         """
-        if any((an,bn)) > len(self.beakers) or any((an,bn)) < 0:
+        self.moved = False
+
+        # Check index
+        if any((an, bn)) > len(self.beakers) or any((an, bn)) < 0:
             if DEBUG:
                 print('Wrong index')
             return False
 
-        if len(self.beakers[an]) != 0 and len(self.beakers[bn]) != self.beaker_size:
-            # test if the destination in empty
-            if len(self.beakers[bn]) == 0:
-                return self.__move(an,bn)
 
-            # test color
-            elif self.beakers[an][-1] == self.beakers[bn][-1]:
-                return self.__move(an,bn)
+        dest = self.beakers[bn]
+        orig = self.beakers[an]
 
-        if DEBUG:
-            print('Move not allowed')
-        return False
+        free_space = lambda x: self.beaker_size - len(x)
+
+
+        #Check if origin not empty
+        if len(orig) == 0:
+            print('Trying to pour from empty')
+            return False
+
+        chunk = [list(g) for k, g in groupby(orig[::-1])][0]
+        print('Chunk', chunk)
+        #assert(all_element_eq(chunk))
+
+        destination_last_color = None
+        try: destination_last_color = self.beakers[bn][-1]
+        except: pass
+
+        if destination_last_color != chunk[0]:
+            if free_space(dest) < len(chunk):
+                print('Disallowed')
+                return
+            repeat(self.__move(an, bn), len(chunk))
+
+        # test color
+        elif orig[-1] == dest[-1]:
+            n = free_space(dest) if (free_space(dest) - len(chunk)) else len(chunk)
+            return repeat(self.__move(an,bn), n)
 
     def __repr__(self):
         return '\n'.join(list(map(str,self.beakers)))
 
+def test0(w):
+    # print(w)
+    w.move(1,2)
+    w.move(1,2)
 
 
 if __name__ == '__main__':
     b = [ [1] , [2,1,2], [2,1] ]
     w = Watersort(3, b)
-    # print(w)
-    # w.move(,1)
     # assert (w.move(1,2))
 
     print(w)
